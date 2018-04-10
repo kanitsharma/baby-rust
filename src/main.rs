b9cdc8429a6d927722a93fe2eb6d1faa82f33f20
Kanit Sharma <kanit.sharma@praktice.ai>
2018-04-10 22:22:45 +0530

    Binding to text message

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
   let (x, y) = (1, 10); // Patterns
-  let z: i32 = 10;
-  println!("{} {} {}", x, y, z);
+  let z: i32 = 10; // types
+  let mut a = 20; // Mutable Variable
+  let a = 10; // bound to int
+  let a = "Hello"; // bound to string now
+  println!("{}", a); // Hello
 }
